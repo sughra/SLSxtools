6cd4276962e3f087257a69526ebb44045d0d38b6
Nicolas Fournel <nicolas.fournel@imag.fr>
2012-02-04 20:39:11 +0100

    Better color handling

--- tests/build_test.sh
+++ tests/build_test.sh
@@ -1,33 +1,78 @@
 #! /bin/bash 
 
+
+# #############################################################################
+#  Print Functions
+# #############################################################################
+
+if [ -t 1 ] ; then # stdout in terminal
+	case "$(tput colors)" in
+		256)
+			INFO_LINE_COLOR="$(tput bold)$(tput setaf 226)" # Light Yellow
+			INFO_TEXT_COLOR="$(tput bold)$(tput setaf 63)"  # Blue
+			
+			ERROR_LINE_COLOR="$(tput bold)$(tput setaf 9)"  # Red
+			ERROR_TEXT_COLOR="$(tput bold)$(tput setaf 9)"  # Red
+			NO_COLOR="$(tput sgr0)"
+			;;
+		*)
+			INFO_LINE_COLOR="$(tput bold)$(tput setaf 3)"   # Yellow
+			INFO_TEXT_COLOR="$(tput bold)$(tput setaf 6)"   # Blue
+			
+			ERROR_LINE_COLOR="$(tput bold)$(tput setaf 2)"  # Red
+			ERROR_TEXT_COLOR="$(tput bold)$(tput setaf 2)"  # Red
+			NO_COLOR="$(tput sgr0)"
+			;;
+	esac
+else # stdout in file !!!
+	INFO_LINE_COLOR=""
+	INFO_TEXT_COLOR=""
+	
+	ERROR_LINE_COLOR=""
+	ERROR_TEXT_COLOR=""
+	
+	NO_COLOR=""
+fi
+
 print_greatings()
 {
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo -en "+"
-    echo -en "\033[01;32m DNAOS Toolchain Testing                               \033[00m"
-    echo "+"
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -en ${INFO_LINE_COLOR}
+    echo     "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -n  "+"
+    echo -en ${INFO_TEXT_COLOR}
+	echo -n  " DNAOS Toolchain Testing                              "
+    echo -en ${INFO_LINE_COLOR}
+    echo     "+"
+    echo     "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -en ${NO_COLOR}
 }
 
 print_step()
 {
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo -en "\033[01;32m"
-	echo " "$1
-	echo -en "\033[00m"
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -en ${INFO_LINE_COLOR}
+    echo     "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -en ${INFO_TEXT_COLOR}
+	echo     " "$1
+    echo -en ${INFO_LINE_COLOR}
+    echo     "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo -en ${NO_COLOR}
 }
 
 error()
 {
-	echo -en "\033[01;31m"
-    echo "!---------------------------------!"
-    echo "! ERROR $1"
-    echo "!---------------------------------!"
-    echo -en " \033[00m"
+    echo -en ${ERROR_LINE_COLOR}
+    echo     "!---------------------------------!"
+    echo     "! ERROR $1"
+    echo     "!---------------------------------!"
+    echo -en ${NO_COLOR}
 
+	false
 	exit
 }
+
+# #############################################################################
+#  Test Functions
+# #############################################################################
 
 BASEDIR=${PWD}
 
